7a89e1e630d76505c20bd3f70c546b3173e9483b
Anjanay Raina <60835814+anjanayraina@users.noreply.github.com>
2024-12-04 12:45:44 +0530

    AggregatorV3Interface added

--- src/Core/VertexFeeVault.sol
+++ src/Core/VertexFeeVault.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "../Interfaces/AggregatorV3Interface.sol";
 
 contract VertexFeeVault is ERC4626, Ownable {
     using SafeERC20 for IERC20;
